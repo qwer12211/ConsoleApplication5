aef6c2d9b9a737218e4ef9e00dc292ad04dbec0a
qwer12211 <145342155+qwer12211@users.noreply.github.com>
2024-06-01 11:22:00 +0300

    Update ConsoleApplication5.cpp

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -1,314 +1,238 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <Windows.h>
+#include <windows.h>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Student {
-private:
-    string groupName;
-    vector<string> subjects;
-    int sdanixRabor;
-    int neSdanixRabot;
 public:
-    Student(string group) : groupName(group), sdanixRabor(0), neSdanixRabot(0) {}
+    string name;
+    string group;
+    vector<string> disciplines;
+    int completed_works;
+    int debts;
 
-    vector<string> getSubjects() const {
-        return subjects;
-    }
+    Student(string name, string group) : name(name), group(group), completed_works(0), debts(0) {}
 
-    void addSubject(string subject) {
-        if (subjects.end() == find(subjects.begin(), subjects.end(), subject))
-            subjects.push_back(subject);
-        else
-            cout << "Дисциплина \"" << subject << "\" уже есть у студента" << endl;
-    }
-
-    void addSdanixRabor() {
-        sdanixRabor++;
-    }
-
-    void addNesdanixRabot() {
-        neSdanixRabot++;
-    }
-
-    void yaProshauTeba() {
-        if (neSdanixRabot > 0) {
-            neSdanixRabot--;
-        }
-        else {
-            neSdanixRabot = neSdanixRabot;
-        }
-    }
-
-    void getSdanixRabor() {
-        if (sdanixRabor > 0) {
-            sdanixRabor--;
-        }
-        else {
-            sdanixRabor = sdanixRabor;
-        }
-    }
-
-    int getNeSdanixRabot() const {
-        return neSdanixRabot;
-    }
-
-    string getGroupName() const {
-        return groupName;
-    }
-
-    int getCompletedAssignments() const {
-        return sdanixRabor;
+    void addDiscipline(string discipline) {
+        disciplines.push_back(discipline);
     }
 };
 
 class Teacher {
-private:
-    string subject;
-    vector<string> groups;
 public:
-    Teacher(const string& subject) : subject(subject) {}
-
-    void addGroup(const string& group) {
-        groups.push_back(group);
-    }
-
-    vector<string> getGroups() const {
-        return groups;
-    }
-
-    string getSubject() const {
-        return subject;
-    }
-};
-
-class Group {
-private:
     string name;
-    vector<Student> students;
-    vector<Teacher> teachers;
-public:
-    Group(const string name) : name(name) {}
+    vector<string> disciplines;
+    vector<string> groups;
 
-    vector<Student> getStudents() {
-        return students;
+    Teacher(string name) : name(name) {}
+
+    void addDiscipline(string discipline) {
+        disciplines.push_back(discipline);
     }
 
-    void addStudent(const Student& student) {
-        students.push_back(student);
-    }
-
-    void addTeacher(const Teacher& teacher) {
-        teachers.push_back(teacher);
-    }
-
-    string getName() const {
-        return name;
+    void addGroup(string group) {
+        groups.push_back(group);
     }
 };
 
 class MPT {
-private:
-    vector<Teacher> teachers;
 public:
-    vector<Group> groups;
+    vector<Student> students;
+    vector<Teacher> teachers;
 
-    void addGroup(const Group group) {
-        groups.push_back(group);
+    void addStudentToGroup(Student& student, string group) {
+        student.group = group;
+        students.push_back(student);
     }
 
-    void addTeacher(const Teacher teacher) {
-        teachers.push_back(teacher);
+    void incrementCompletedWork(Student& student) {
+        student.completed_works++;
     }
 
-    void addWork(Student student) {
-        student.addSdanixRabor();
+    void addDisciplineToTeacher(Teacher& teacher, string discipline) {
+        teacher.addDiscipline(discipline);
     }
 
-    void addDolg(Student student) {
-        student.addNesdanixRabot();
+    void addGroupToTeacher(Teacher& teacher, string group) {
+        teacher.addGroup(group);
     }
 
-    void showStudentInGroup(Group& group) {
-        cout << "Количество студентов в группе " << group.getName() << ": " << group.getStudents().size() << endl;
+    void submitWork(Student& student) {
+        student.completed_works++;
+        student.debts--;
     }
 
-    void showStudentInfo(const Student student) {
-        cout << "Информация о студенте:" << endl;
-        cout << "Название группы: " << student.getGroupName() << endl;
-        cout << "Дисциплины: ";
-        for (const auto& subject : student.getSubjects()) {
-            cout << subject << ", ";
+    int countStudentsInClass(string group) {
+        int count = 0;
+        for (const auto& student : students) {
+            if (student.group == group)
+                count++;
         }
-        cout << endl;
-        cout << "Количество выполненных работ: " << student.getCompletedAssignments() << endl;
-        cout << "Количество долгов: " << student.getNeSdanixRabot() << endl;
-    }
-
-    void showAllStudentsInfo() {
-        for (auto& group : groups) {
-            for (auto& student : group.getStudents()) {
-                showStudentInfo(student);
-                cout << endl;
-            }
-        }
-    }
-
-    void showAllGroups() const {
-        cout << "Все группы:" << endl;
-        for (const auto& group : groups) {
-            cout << group.getName() << endl;
-        }
-    }
-
-    bool groupExists(const string& group_name) const {
-        for (const auto& group : groups) {
-            if (group.getName() == group_name) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool subjectExists(const string& subject) {
-        for (auto& group : groups) {
-            for (auto& student : group.getStudents()) {
-                if (find(student.getSubjects().begin(), student.getSubjects().end(), subject) != student.getSubjects().end())
-                    return true;
-            }
-        }
-        return false;
+        return count;
     }
 };
+
+
 
 int main() {
     setlocale(LC_ALL, "RUS");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(0, "");
-    MPT MPT;
 
-    string group_name;
-    cout << "\nВведите название группы: ";
-    cin >> group_name;
-
-    Group group(group_name);
-
-    Student student1(group_name);
-
-    string subject;
-    cout << "Введите дисциплину для группы: ";
-    cin >> subject;
-    student1.addSubject(subject);
-
-    Teacher teacher1(subject);
-
-    string groupOfTeacher;
-
-    do {
-        cout << "Введите название группы для преподователя: ";
-
-        cin >> groupOfTeacher;
-
-        if (groupOfTeacher != group_name) {
-            cout << "Группы с таким названием не существует! Пожайлуста, попробуйте ещё раз! \n";
-        }
-    } while (groupOfTeacher != group_name);
-
-    teacher1.addGroup(groupOfTeacher);
-    group.addStudent(student1);
-    group.addTeacher(teacher1);
-    MPT.addGroup(group);
-    MPT.addTeacher(teacher1);
-    char choice;
-
-    do {
-        cout << " \n1 - Добавить работу группе\n 2 - Добавить долг группе\n 3 - Посмотреть количество студентов в группе\n 4 - Показать информацию о группе\n 5 - Добавить студента в группу\n 6 - Добавить новую группу\n 7 - Показать все группы\n 8 - Добавить новую дисциплину\n 9 - Удалить долги группе\n 10 - Отметить практическую работу выполненной\n 11 - Завершить\n";
+    MPT mpt;
+    while (true) {
+        cout << "Выберите операцию:\n"
+            << "1. Добавить студента\n"
+            << "2. Добавить преподавателя\n"
+            << "3. Добавить дисциплину преподавателю\n"
+            << "4. Добавить группу преподавателю\n"
+            << "5. Сдать работу\n"
+            << "6. Добавить долг\n"
+            << "7. Показать студентов\n"
+            << "8. Выход\n";
+        int choice;
         cin >> choice;
-
         switch (choice) {
-        case '1':
-            cout << "\nГруппе был добвален практос\n";
-            MPT.addWork(student1);
-            break;
-        case '2':
-            cout << "\nГруппе был добавлен долг\n";
-            MPT.addDolg(student1);
-            break;
-        case '3':
-            MPT.showStudentInGroup(group);
-            break;
-        case '4':
-            MPT.showStudentInfo(student1);
-            break;
-        case '5':
-        {
-            string newStudentGroup;
-            cout << "\nВведите название группы для  студента";
-            cin >> newStudentGroup;
+        case 1: {
+            cout << "Введите имя студента: ";
+            string name;
+            cin >> name;
+            cout << "Введите группу студента: ";
+            string group;
+            cin >> group;
 
-            if (!MPT.groupExists(newStudentGroup)) {
-                cout << "\nГруппы с таким названием нет\n" << endl;
+            bool studentExists = false;
+            for (const auto& student : mpt.students) {
+                if (student.name == name) {
+                    studentExists = true;
+                    break;
+                }
+            }
+
+
+            if (studentExists) {
+                cout << "Студент с таким именем уже существует." << endl;
             }
             else {
-                Student newStudent(newStudentGroup);
-                group.addStudent(newStudent);
+                Student student(name, group);
+                mpt.addStudentToGroup(student, group);
             }
             break;
         }
-        case '6':
-        {
-            string newGroup;
-            cout << "\nВведите название новой группы: ";
-            cin >> newGroup;
 
-            if (MPT.groupExists(newGroup)) {
-                cout << "\nГруппа с таким названием уже существует\n" << endl;
-            }
-            else {
-                Group new_Group(newGroup);
-                MPT.addGroup(new_Group);
+        case 2: {
+            cout << "Введите имя преподавателя: ";
+            string name;
+            cin >> name;
+            Teacher teacher(name);
+            mpt.teachers.push_back(teacher);
+            break;
+        }
+        case 3: {
+            cout << "Введите имя преподавателя: ";
+            string teacher_name;
+            cin >> teacher_name;
+            cout << "Введите дисциплину: ";
+            string discipline;
+            cin >> discipline;
+            for (auto& teacher : mpt.teachers) {
+                if (teacher.name == teacher_name) {
+                    bool disciplineExists = false;
+                    for (const auto& existingDiscipline : teacher.disciplines) {
+                        if (existingDiscipline == discipline) {
+                            disciplineExists = true;
+                            break;
+                        }
+                    }
+                    if (!disciplineExists) {
+                        teacher.addDiscipline(discipline);
+                    }
+                    else {
+                        cout << "Дисциплина уже добавлена преподавателю." << endl;
+                    }
+                    break;
+                }
             }
             break;
         }
-        case '7':
-            MPT.showAllGroups();
-            break;
-        case '8':
-        {
-            string newSubject;
-            cout << "\nВведите название новой дисциплины: ";
-            cin >> newSubject;
 
-            student1.addSubject(newSubject);
-
-            for (Student& student : group.getStudents()) {
-                student.addSubject(newSubject);
+        case 4: {
+            cout << "Введите имя преподавателя: ";
+            string teacher_name;
+            cin >> teacher_name;
+            cout << "Введите группу: ";
+            string group;
+            cin >> group;
+            bool group_exists = false;
+            bool teacher_exists = false;
+            for (auto& teacher : mpt.teachers) {
+                if (teacher.name == teacher_name) {
+                    teacher_exists = true;
+                    for (const auto& existing_group : teacher.groups) {
+                        if (existing_group == group) {
+                            group_exists = true;
+                            break;
+                        }
+                    }
+                    if (group_exists) {
+                        cout << "Группа уже существует у преподавателя." << endl;
+                    }
+                    else {
+                        teacher.addGroup(group);
+                        cout << "Группа успешно добавлена." << endl;
+                    }
+                    break;
+                }
             }
-
-            cout << "Дисциплина добавлена\n" << endl;
-
-            break;
-        }
-        case '9':
-            cout << "\nE группы был удалён один долг";
-            student1.yaProshauTeba();
-            break;
-        case 's':
-            student1.getSdanixRabor();
-            cout << "\nСтуденты выполнили практическую работу\n";
-            break;
-        case 'q':
-            cout << "Вы вышли из программы!";
-            break;
-        default:
-            cout << "\nТакого в меню нет!\n" << endl;
+            if (!teacher_exists) {
+                cout << "Преподаватель не найден." << endl;
+            }
             break;
         }
 
-    } while (choice != 'q');
 
+        case 5: {
+            cout << "Введите имя студента, который сдает работу: ";
+            string student_name;
+            cin >> student_name;
+            for (auto& student : mpt.students) {
+                if (student.name == student_name) {
+                    mpt.submitWork(student);
+                    break;
+                }
+            }
+            break;
+        }
+        case 6: {
+            cout << "Введите имя студента, которому добавляется долг: ";
+            string student_name;
+            cin >> student_name;
+            for (auto& student : mpt.students) {
+                if (student.name == student_name) {
+                    student.debts++;
+                    break;
+                }
+            }
+            break;
+        }
+        case 7: {
+            for (const auto& student : mpt.students) {
+                cout << student.name << " " << student.group << " " << student.completed_works << " " << student.debts << endl;
+            }
+            break;
+        }
+        case 8: {
+            return 0;
+        }
+        default: {
+            cout << "Неверный выбор. Пожалуйста, попробуйте снова.\n";
+            break;
+        }
+        }
+    }
     return 0;
 }
